89be6f0d37997dff511000e393d71a32801b1364
Deep Sukhwani <deepsukhwani@gmail.com>
2015-11-29 10:35:41 +0530

    L4: Hashing Salts

--- makePasswordHash.py
+++ makePasswordHash.py
@@ -10,4 +10,14 @@ def make_salt():
     list_random_chars = random.sample(string.ascii_letters, 5)
     return ''.join(str(x) for x in list_random_chars)
     
-print make_salt()
+# print make_salt()
+
+# implement the function make_pw_hash(name, pw) that returns a hashed password 
+# of the format: 
+# HASH(name + pw + salt),salt
+# use sha256
+
+def make_pw_hash(name, pw):
+    ###Your code here
+    salt = make_salt()
+    return '%s%s%s' % (hashlib.sha256(name + pw + salt).hexdigest(), ',', salt)
